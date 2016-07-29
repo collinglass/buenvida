52e070c913be878e2de72e66598ca3c926f02c79
Collin Glass <collinglass@gmx.com>
2016-07-29 12:06:05 -0400

    crud functionality for memories mapped out, move over to using HAML everywhere.

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,7 +1,40 @@
 class MemoriesController < ApplicationController
   def index
+    @memories = Memory.all
+  end
+  
+  def show
+    @memory = Memory.find(params[:id])
   end
 
   def new
+    @memory = Memory.new
+  end
+
+  def create
+    @memory = Memory.create(memory_params)
+    redirect_to memories_path
+  end
+
+  def edit
+    @memory = Memory.find(params[:id])
+  end
+
+  def update
+    @memory = Memory.find(params[:id])
+    @memory.update(memory_params)
+    redirect_to(memory_path(params[:id]))
+  end
+
+  def destroy
+    @memory = Memory.find(params[:id])
+    @memory.destroy
+    redirect_to memories_path
+  end
+
+  private
+
+  def memory_params
+   params.require(:memory).permit(:image, :caption)
   end
 end
