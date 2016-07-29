c0c2337aaf723a22194aa10c933b365469d620d9
Collin Glass <collinglass@gmx.com>
2016-07-29 13:02:27 -0400

    add alerts on success and failure

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,10 +1,11 @@
 class MemoriesController < ApplicationController
+  before_action :set_memory, only: [:show, :edit, :update, :destroy]
+
   def index
     @memories = Memory.all
   end
   
   def show
-    @memory = Memory.find(params[:id])
   end
 
   def new
@@ -12,29 +13,42 @@ class MemoriesController < ApplicationController
   end
 
   def create
-    @memory = Memory.create(memory_params)
-    redirect_to memories_path
+    if @memory = Memory.create(memory_params)
+      flash[:success] = "Your memory has been created!"
+      redirect_to memories_path
+    else
+      flash.now[:alert] = "Your new memory couldn't be created! Please check the form."
+      render :new
+    end
   end
 
   def edit
-    @memory = Memory.find(params[:id])
   end
 
   def update
-    @memory = Memory.find(params[:id])
-    @memory.update(memory_params)
-    redirect_to(memory_path(params[:id]))
+    if @memory.update(memory_params)
+      flash[:success] = "Your memory has been updated!"
+      redirect_to(memory_path(params[:id]))
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
   end
 
   def destroy
-    @memory = Memory.find(params[:id])
-    @memory.destroy
-    redirect_to memories_path
+    if @memory.destroy
+      flash[:success] = "Your memory has been destoryed!"
+      redirect_to memories_path
+    end
   end
 
   private
 
   def memory_params
    params.require(:memory).permit(:image, :caption)
+  end
+
+  def set_memory
+    @memory = Memory.find(params[:id])
   end
 end
